07ed866889527aaed9df83a90c81e7c8b5c54907
CamperGit <sashchenkodmitrii@mail.ru>
2021-09-19 16:15:53 +0300

    git init

---
+++ src/main/java/com/millenium/milleniumserver/MilleniumServerApplication.java
@@ -0,0 +1,13 @@
+package com.millenium.milleniumserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilleniumServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilleniumServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/millenium/milleniumserver/MilleniumServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.millenium.milleniumserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilleniumServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
