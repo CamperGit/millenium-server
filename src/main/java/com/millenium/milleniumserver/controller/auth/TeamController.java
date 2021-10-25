a0ec80be8ea5814b634223216efe4e712c53b0f8
CamperGit <sashchenkodmitrii@mail.ru>
2021-10-25 18:57:22 +0300

    delete useless code

--- src/main/java/com/millenium/milleniumserver/controller/auth/TeamController.java
+++ src/main/java/com/millenium/milleniumserver/controller/auth/TeamController.java
@@ -1,15 +1,12 @@
 package com.millenium.milleniumserver.controller.auth;
 
 import com.millenium.milleniumserver.entity.auth.TeamEntity;
-import com.millenium.milleniumserver.payload.requests.TestNumberRequest;
 import com.millenium.milleniumserver.services.auth.TeamEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 @CrossOrigin(origins = "${crossOrigin.url}", maxAge = 3600)
 @RestController
@@ -23,16 +20,8 @@ public class TeamController {
     }
 
     @GetMapping
-    public TeamEntity getTeamById(@RequestParam String name, @RequestParam Integer number) {
-        System.out.println(name);
-        System.out.println(number);
-        return teamEntityService.getTeamEntityById(number);
-    }
-
-    @PostMapping(value = "/test")
-    public void testRequestEntity(@RequestBody @Valid TestNumberRequest request) {
-        System.out.println(request.getName());
-        System.out.println(request.getNumber());
+    public TeamEntity getTeamById(@RequestParam Integer teamId) {
+        return teamEntityService.getTeamEntityById(teamId);
     }
 
     @Autowired
